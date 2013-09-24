111056db36844a577a2b78b4a04d1713a5d4430f
Donald Stufft <donald@stufft.io>
2013-09-24 07:50:30 -0400

    Add more tests

--- tests/test_simple_index.py
+++ tests/test_simple_index.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import urllib.parse
 
 
 def test_api_version(request):
@@ -25,3 +26,43 @@ def test_api_version(request):
     assert resp.status_code == 200
     assert resp.headers["Content-Type"].split(";")[0] == "text/html"
     assert resp.html.xpath("//meta[@name='api-version']/@value") == ["2"]
+
+
+def test_simple_index_looks_like_list_of_names(request):
+    resp = request.get("")
+
+    assert resp.status_code == 200
+    assert resp.headers["Content-Type"].split(";")[0] == "text/html"
+    assert "pypi.testpkg" in resp.html.xpath("//a[@href]/text()")
+
+
+def test_api_version_package_page(request):
+    resp = request.get("pypi.testpkg/")
+
+    assert resp.status_code == 200
+    assert resp.headers["Content-Type"].split(";")[0] == "text/html"
+    assert resp.html.xpath("//meta[@name='api-version']/@value") == ["2"]
+
+
+def test_package_page_redirect(request):
+    resp = request.get("pypi.testpkg", allow_redirects=False)
+
+    assert resp.status_code in {301, 302, 303, 307}
+    assert resp.headers["Location"].endswith("/pypi.testpkg/")
+
+
+def test_package_page_version_urls(request):
+    resp = request.get("pypi.testpkg/")
+
+    filenames = set(
+        urllib.parse.urlparse(url).path.rsplit("/", 1)[-1]
+        for url in resp.html.xpath("//a[@rel='internal']/@href")
+    )
+
+    assert resp.status_code == 200
+    assert resp.headers["Content-Type"].split(";")[0] == "text/html"
+    assert filenames == {
+        "pypi.testpkg-1.0.tar.gz",
+        "pypi.testpkg-1.5.tar.gz",
+        "pypi.testpkg-2.0.tar.gz",
+    }
